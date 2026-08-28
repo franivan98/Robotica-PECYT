26d946494eed367a210e4d973d21d552f80ccfab
Nicolás Oviedo <ignacio.oviedo.239@mi.unc.edu.ar>
2026-08-28 11:07:12 -0300

    mejora al codigo de prueba general

--- Mantenimiento/TestGeneral.ino
+++ Mantenimiento/TestGeneral.ino
@@ -3,56 +3,126 @@
  // pin 9 echo
  // 11 a 13 pines de leds (semaforo) solo programamos con 11 y 12, 13 funciona a la par con el led integrado en el arduino
  
- // funcion ultrasonico distancia
+ // Pines LED
+#define GREEN_LED 11
+#define YELLOW_LED 12
+#define RED_LED 13
+//  Pines ultrasonico
+#define TRIG_PIN 8
+#define ECHO_PIN 9
+//  Pines motores
+//  Motor A
+#define MOTORA_1 3
+#define MOTORA_2 4
+//  Motor B
+#define MOTORB_1 5
+#define MOTORB_2 6
+
+ // Funcion ultrasonico distancia
  long ultrason_8() {
    long duration, distance;
-   digitalWrite(8,LOW);
+   digitalWrite(TRIG_PIN,LOW);
    delayMicroseconds(2);
-   digitalWrite(8, HIGH);
+   digitalWrite(TRIG_PIN, HIGH);
    delayMicroseconds(10);
-   digitalWrite(8, LOW);
-   duration = pulseIn(9, HIGH);
+   digitalWrite(TRIG_PIN, LOW);
+   duration = pulseIn(ECHO_PIN, HIGH);
    distance = duration/58;
    return distance;
 }
 
-// funcion motores con sensor ultrasonico
-void Motor_y_Sensores() {
+// Led VERDE (11) encendido (apaga el resto):
+void only_green(){
+  digitalWrite(GREEN_LED, 1);
+  digitalWrite(YELLOW_LED, 0);
+  digitalWrite(RED_LED, 0);
+}
+
+// Led AMARILLO (12) encendido (apaga el resto):
+void only_yellow(){
+  digitalWrite(GREEN_LED, 0);
+  digitalWrite(YELLOW_LED, 1);
+  digitalWrite(RED_LED, 0);
+}
+
+// Led ROJO (13) encendido (apaga el resto):
+void only_red(){
+  digitalWrite(GREEN_LED , 0);
+  digitalWrite(YELLOW_LED, 0);
+  digitalWrite(RED_LED, 1);
+}
+
+// Función ADELANTE
+void forward(){
+  // Rueda IZQUIERDA:
+  digitalWrite(6, 0);
+  digitalWrite(5, 1);
+  // Rueda DERECHA:
+  digitalWrite(4, 0);
+  digitalWrite(3, 1);
+  // Enciende verde
+  //only_green();
+}
+
+// Función GIRAR
+void turn(){
+  // Rueda IZQUIERDA:
+  digitalWrite(MOTORA_1, 1);
+  digitalWrite(MOTORA_2, 0);
+  // Rueda DERECHA:
+  digitalWrite(MOTORB_1, 0);
+  digitalWrite(MOTORB_2, 1);
+  // Enciende el amarillo
+  //only_yellow();
+}
+
+// Función PARAR
+void stop(){
+  // Rueda IZQUIERDA:
+  digitalWrite(MOTORA_1, 0);
+  digitalWrite(MOTORA_2, 0);
+  // Rueda DERECHA:
+  digitalWrite(MOTORB_1, 0);
+  digitalWrite(MOTORB_2, 0);
+  // Enciende el rojo
+  //only_red();
+}
+
+// Funcion motores con sensor ultrasonico:
+void dodge() {
+  // Si el sensor está a una distancia MAYOR a (>) 20cm, entonces:
   if (ultrason_8() > 20) {
-    pinMode(6, OUTPUT);
-     digitalWrite(6, 1);
-    pinMode(5, OUTPUT);
-     digitalWrite(5, 0);
-    pinMode(4, OUTPUT);
-     digitalWrite(4, 0);
-    pinMode(3, OUTPUT);
-     digitalWrite(3, 1);
-    digitalWrite(11, 1);
-    digitalWrite(12, 0);
-  } else {
-    pinMode(6, OUTPUT);
-     digitalWrite(6, 0);
-    pinMode(5, OUTPUT);
-     digitalWrite(5, 1);
-    pinMode(4, OUTPUT);
-     digitalWrite(4, 1);
-    pinMode(3, OUTPUT);
-     digitalWrite(3, 0);
-    digitalWrite(11, 0);
-    digitalWrite(12, 1);
+    //Va hacia ADELANTE
+    forward();
+  } 
+  // De lo contrario (si está a una distancia MENOR a 20cm):
+  else {
+    // Que ESPERE y prenda el LED ROJO por 2 SEGUNDOS:
+    stop();
+    delay(1000);
+    // Que GIRE y prenda el LED AMARILLO por 2 SEGUNDOS:
+    turn();
+    delay(75);
   }
 }
 
 void setup() {
+  // Motores
+  pinMode(MOTORA_1, OUTPUT);
+  pinMode(MOTORA_2, OUTPUT);
+  pinMode(MOTORB_1, OUTPUT);
+  pinMode(MOTORB_2, OUTPUT);
+
   // Ultrasonico
-  pinMode(8, OUTPUT);
-  pinMode(9, INPUT);
+  pinMode(TRIG_PIN, OUTPUT);
+  pinMode(ECHO_PIN, INPUT);
+
   // Leds
-  pinMode(13, OUTPUT);
-  pinMode(12, OUTPUT);
-  pinMode(11, OUTPUT);
+  pinMode(RED_LED, OUTPUT);
+  pinMode(YELLOW_LED, OUTPUT);
+  pinMode(GREEN_LED, OUTPUT);
 }
 
 void loop() {
-    Motor_y_Sensores();
+    dodge();
 }
